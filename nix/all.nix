6604a9746cfea510fda897078b719984c77e03ea
Lennart Spitzner <hexagoxel@hexagoxel.de>
2020-04-18 16:50:15 +0200

    Major changeset: Add applicative, Refactor monadic interface
    - monadic interface now uses two-phase setup: First step is to create a
      full CommandDesc value, second is running the parser on input while the
      CommandDesc is chained along
    - applicative interface has a somewhat nicer/cleaner implementation, is
      more secure by avoiding any demands on the API user that are not encoded
      in types, but is slightly less expressive and requires ApplicativeDo to
      get readable code.
    - The applicative interface is *NOT* finished and the test-suite does not
      cover it.
    - Add the `traverseBarbie` construct which introduces a dependency on
      the `barbies` library. This also effectively:
    - Stop support for ghc < 8.4.
    - Refactor the module structure a bit, and change the API of the central
      `runCmdParser` function. It now returns a `PartialParseInfo`. Essentially,
      `runCmdParser` is a combination of the previous `runCmdParser` and the
      previous `simpleCompletion`. This API design is a curious advantage to
      laziness: Returning a complex struct is harmless as fields that the user
      does not use won't be evaluated. The downside is that the core function now
      looks like a complex beast, but the upside is that there is no need to
      expose multiple functions that are supposed to be chained in a certain way
      to get all functionality (if desired), and we still _can_ provide simpler
      versions that are just projections on the `PartialParseInfo`.
    - Stop support for an anti-feature: The implicit merging of multiple
      sub-commands definitions with the same name.

--- nix/all.nix
+++ nix/all.nix
@@ -59,21 +59,21 @@ let
     });
     "hackage-8.4" = import ./via-hackage.nix (args // { 
       ghc-ver = "ghc844";
-      index-state = "2020-05-01T00:00:00Z";
+      index-state = "2020-08-10T00:00:00Z";
       # plan-sha256 = "0s6rfanb6zxhr5zbinp7h25ahwasciwj3ambsr6zdxm1l782b3ap";
       # materialized = ./materialized/hackage-8.4;
       configureArgs = "--allow-newer='multistate:*'";
     });
     "hackage-8.6" = import ./via-hackage.nix (args // { 
       ghc-ver = "ghc865";
-      index-state = "2020-05-01T00:00:00Z";
+      index-state = "2020-08-10T00:00:00Z";
       # plan-sha256 = "01m95xirrh00dvdxrpsx8flhcwlwcvgr3diwlnkw7lj5f3i7rfrl";
       # materialized = ./materialized/hackage-8.6;
       configureArgs = "--allow-newer='multistate:*'";
     });
     "hackage-8.8" = import ./via-hackage.nix (args // { 
       ghc-ver = "ghc883";
-      index-state = "2020-05-01T00:00:00Z";
+      index-state = "2020-08-10T00:00:00Z";
       # plan-sha256 = "14qs7ynlf7p2qvdk8sf498y87ss5vab3ylnbpc8sacqbpv2hv4pf";
       # materialized = ./materialized/hackage-8.8;
       configureArgs = "--allow-newer='multistate:*'";
@@ -82,7 +82,7 @@ let
     then {
     "hackage-8.10" = import ./via-hackage.nix (args // { 
       ghc-ver = "ghc8101";
-      index-state = "2020-06-06T00:00:00Z";
+      index-state = "2020-08-10T00:00:00Z";
       # index-sha256 = "1h1x65840jl6w2qvyq9csc7b3ivadr933glarnmydk2b23vw2i77";
       # plan-sha256 = "1s8a6cb5qgf4ky5s750rzx6aa52slp1skazh8kbx0dbfjd6df7yw";
       # materialized = ./materialized/hackage-8.10;
